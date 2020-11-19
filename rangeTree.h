5b339a96e04c09178aa9592ba7b812f2dac62739
Piero Morales <piero.morales@utec.edu.pe>
2020-11-18 23:01:50 -0500

    Corrección de errores

--- rangeTree.h
+++ rangeTree.h
@@ -1,68 +1,67 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 typedef pair<int, int> pii;
 
-
-struct Nodo
-{
+struct Nodo {
     int x;
-    Nodo* m_pSon[3];
+    Nodo* m_pSon[3]{};
     Nodo* next;
     Nodo* prev;
     bool isLeaf;
     
-    Nodo(int n, bool is_leaf = true)
-    {
-    x = n;
-    m_pSon[0]=0;
-    m_pSon[1]=0; 
-    m_pSon[2]=0; 
-    next = prev = 0;
-    isLeaf = is_leaf;
+    explicit Nodo(int n, bool is_leaf = true) {
+        x = n;
+        m_pSon[0] = nullptr;
+        m_pSon[1] = nullptr;
+        m_pSon[2] = nullptr;
+        next = prev = nullptr;
+        isLeaf = is_leaf;
     }   
 };
 
-
 bool comparex(pii a, pii b) {
-  return (a.first < b.first);
+    return (a.first < b.first);
 }
 
 bool comparey(pii a, pii b) {
-  return (a.second < b.second);
+    return (a.second < b.second);
 }
 
-Nodo* get_most_right(Nodo* parent){
-  	while (!parent->isLeaf){
-		if(parent->m_pSon[2]) parent = parent->m_pSon[2];
-		else parent = parent->m_pSon[0];
+Nodo* get_most_right(Nodo* parent) {
+  	while (!parent->isLeaf) {
+		if(parent->m_pSon[2]) {
+		    parent = parent->m_pSon[2];
+		} else {
+            parent = parent->m_pSon[0];
+		}
     }
   	return parent;
 }
 
-Nodo* get_most_left(Nodo* parent){
-	while (!parent->isLeaf){
-		if(parent->m_pSon[0]) parent = parent->m_pSon[0];
-		else parent = parent->m_pSon[2];
+Nodo* get_most_left(Nodo* parent) {
+	while (!parent->isLeaf) {
+		if(parent->m_pSon[0]) {
+		    parent = parent->m_pSon[0];
+		} else {
+		    parent = parent->m_pSon[2];
+		}
     }
   	return parent;
 }
-
 
 Nodo* create_range_tree(vector <pii> v, int l, int h, bool axis=true) {
-    if(l==h)
-    {
+    if (l==h) {
         Nodo* padre;
         int value = axis? v[l].first: v[l].second;
         padre = new Nodo(value, false);
         Nodo* child = new Nodo(value);
         padre->m_pSon[0] = child;
-        
         return padre;
-    } 
-
-    if( l+1==h) {
+    }
+    if (l+1==h) {
         Nodo* padre;
         int value_left = axis? v[l].first: v[l].second;
         int value_right = axis? v[h].first: v[h].second;
@@ -87,13 +86,10 @@ Nodo* create_range_tree(vector <pii> v, int l, int h, bool axis=true) {
 	child_most_right->next = child_most_left;
 	child_most_left->prev = child_most_right;
 
-	Nodo * child_left_y = nullptr;
-	Nodo * child_right_y = nullptr;
-
 	if (axis) {
-		child_left_y = create_range_tree(v,l, m, false);
+        Nodo* child_left_y = create_range_tree(v,l, m, false);
 		child_left_x->m_pSon[1] = child_left_y;
-		child_right_y = create_range_tree(v,m+1, h, false);
+        Nodo* child_right_y = create_range_tree(v,m+1, h, false);
 		child_right_x->m_pSon[1] = child_right_y;
 	}
     
